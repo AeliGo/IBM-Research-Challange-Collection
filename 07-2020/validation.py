74be88a8241003ebf28eecf7b23e68e7baa11576
LI WANG <zoeywang1990@gmail.com>
2020-07-05 18:02:27 +1000

    typo

--- 07-2020/validation.py
+++ 07-2020/validation.py
@@ -13,17 +13,17 @@ def getFibonacciList(limit):
 def getGoldenRectangleNumber(n):
     if n < 1:
         print("Incorrect Golden Rectangle Number Input")
-    
+
     return FibonacciList[n] * FibonacciList[n + 1]
 
 
 def calculateTotalCount(initialMatrix):
     total = 0
     subMatrix = []
-    for i in range(len(matrix_b)):
+    for i in range(len(evolutionMatrix)):
         subTotal = 0
-        for j in range(len(matrix_b[0])):
-            subTotal += matrix_b[i][j] * initialMatrix[j]
+        for j in range(len(evolutionMatrix[0])):
+            subTotal += evolutionMatrix[i][j] * initialMatrix[j]
         subMatrix.append(subTotal)
         total += subTotal
     for k in range(len(subMatrix)):
@@ -32,7 +32,7 @@ def calculateTotalCount(initialMatrix):
 
 
 def validation(limit):
-    initialMatrix = [1, 0, 0, 0, 0]
+    initialMatrix = initial
     for count in range(limit):
         if count > 1:
             if calculateTotalCount(initialMatrix) != getGoldenRectangleNumber(
@@ -42,10 +42,15 @@ def validation(limit):
             else:
                 print("correct at ", count)
 
+
 FibonacciList = getFibonacciList(10000)
 
+matrix_initial_a = [1, 0, 0, 0]
+matrix_initial_b = [1, 0, 0, 0, 0]
 matrix_a = [[1, 1, 1, 0], [1, 1, 1, 1], [0, 1, 0, 0], [0, 1, 0, 0]]
 matrix_b = [[0, 1, 2, 1, 0], [1, 0, 0, 1, 0], [1, 0, 1, 0, 0], [0, 1, 0, 1, 1],
             [0, 0, 1, 1, 0]]
+evolutionMatrix = matrix_b
+initial = matrix_initial_b
 
 validation(9000)

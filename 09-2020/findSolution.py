1cd4e69493cdc6995de61e533e4cbd643281b94c
LI WANG <zoeywang1990@gmail.com>
2020-09-13 21:42:53 +1000

    9work but 11 fail

--- 09-2020/findSolution.py
+++ 09-2020/findSolution.py
@@ -1,4 +1,7 @@
 import json
+from itertools import permutations
+
+from dataset import dataset9, dataset11, dataset12
 
 # reference:
 # http://uplsj.com/wp-content/uploads/2020/09/rps.pdf
@@ -13,6 +16,7 @@ class solution:
     threeCyclesMap = {}
     profileMap = {}
     chunkedVertex = []
+    am = []
 
     def __init__(self, dimension, dataset):
         self.dimension = dimension
@@ -20,77 +24,26 @@ class solution:
 
     def run(self):
         for data in self.dataset:
-            self.generateRelationMap(str(data)).find3cycle().getCycles(
-            ).generateProfile().getChunkedVertex()
-            print(
-                "-----------------------------------------------------------------------------------------------------------------------"
-            )
+            #clear am container
+            self.am = []
 
-    def getCycles(self):
-        self.threeCycles = []
-        for i in range(self.dimension):
-            for cycle in self.threeCyclesMap[i]:
-                self.checkIsNotExist(cycle) and self.threeCycles.append(cycle)
-        return self
-
-    def checkIsNotExist(self, cycle):
-        for i in range(len(cycle)):
-            cc = cycle + cycle
-            if cc[i:i + len(cycle)] in self.threeCycles:
-                return False
-        return True
-
-    def generateProfile(self):
-        for i in range(self.dimension):
-            self.profileMap[i] = {}
-            for item in self.threeCyclesMap[i]:
-                ss = item + item
-                counts = []
-                for edge in [ss[0:2], ss[1:3], ss[2:4]]:
-                    counts.append(str(self.getEdgeInCycleCount(edge)))
-                key = "".join(counts)
-                self.profileMap[i][key] = self.profileMap[i][
-                    key] + 1 if key in self.profileMap[i] else 1
-                self.profileMap[i] = dict(sorted(self.profileMap[i].items()))
-        return self
-
-    def getChunkedVertex(self):
-        self.chunkedVertex = []
-        pointProfileDict = {}
-
-        for key, value in self.profileMap.items():
-            valueInJson = json.dumps(value)
-            if valueInJson in pointProfileDict:
-                pointProfileDict[valueInJson].append(key)
-            else:
-                pointProfileDict[valueInJson] = [key]
-        self.chunkedVertex = list(pointProfileDict.values())
-        return self
-
-    def getEdgeInCycleCount(self, edge):
-        count = 0
-        for cycle in self.threeCycles:
-            if edge[0] in cycle and edge[1] in cycle:
-                count += 1
-        return count
+            self.generateRelationMap(str(data))
+            self.generateCycleMap()
+            self.getCycles()
+            self.generateProfile()
+            self.getChunkedVertices()
+            self.searchAutomorphism(self.chunkedVertex, 0,
+                                    [i for i in range(self.dimension)])
+            if len(self.am) > 1:
+                print("Total Ams:", len(self.am))
+                print("Solution:", self.relationMap)
+                print("----------------------------------------------------")
 
     def generateRelationMap(self, data):
         splitData = self.splitStr(self.dimension - 1, data, [])
         self.fillMap(splitData)
-        return self
 
-    def find3cycle(self):
-        for i in range(self.dimension):
-            self.threeCyclesMap[i] = []
-            levelOne = self.relationMap[i]
-            for ii in levelOne:
-                levelTwo = self.relationMap[ii]
-                for iii in levelTwo:
-                    i in self.relationMap[iii] and self.threeCyclesMap[
-                        i].append(str(i) + str(ii) + str(iii))
-        return self
-
-    # method used to split original data
+    # method to split original data
     # "111100001110001101011101110111000000"  => ['11110000', '1110001', '101011', '10111', '0111', '000', '00', '0']
     def splitStr(self, D, originalStr, container=[]):
         if D == 0:
@@ -111,19 +64,88 @@ class solution:
                 else:
                     self.relationMap[y].append(x)
 
+    def generateCycleMap(self):
+        for i in range(self.dimension):
+            self.threeCyclesMap[i] = []
+            levelOne = self.relationMap[i]
+            for ii in levelOne:
+                levelTwo = self.relationMap[ii]
+                for iii in levelTwo:
+                    i in self.relationMap[iii] and self.threeCyclesMap[
+                        i].append(str(i) + str(ii) + str(iii))
 
-dataset = [
-    111100001110001101011101110111000000, 111100001011010110101001111011100101,
-    111100001110010101101110110111100101, 111100001111000101011101110111100101,
-    111100001110010011101010110111100101, 111100001010011110011011011110100010,
-    111100001010011110101011101011001000, 111100001010101110011010111110001000,
-    111100001010110110011101011111000101, 111100001010110110101100111111000101,
-    111100001110100101011101110111000101, 111100001111000100111101110111000101,
-    111100001010110110101010111011001000, 111100001010101110110010111011001000,
-    111100001110001110011101111111000000
-]
+    def getCycles(self):
+        self.threeCycles = []
+        for i in range(self.dimension):
+            for cycle in self.threeCyclesMap[i]:
+                self.checkIsNotExist(cycle) and self.threeCycles.append(cycle)
 
-solution = solution(9, dataset)
+    def checkIsNotExist(self, cycle):
+        for i in range(len(cycle)):
+            cc = cycle + cycle
+            if cc[i:i + len(cycle)] in self.threeCycles:
+                return False
+        return True
 
-# solution = solution(5, [1100101110])
-solution.run()
+    # check is automorphism
+    def checkIsAm(self, permutationIndices):
+        for i in range(self.dimension):
+            for j in range(self.dimension):
+                original = 0 if j in self.relationMap[i] else 1
+                current = 0 if permutationIndices[j] in self.relationMap[
+                    permutationIndices[i]] else 1
+                if original != current:
+                    return False
+        return True
+
+    def generateProfile(self):
+        for i in range(self.dimension):
+            self.profileMap[i] = {}
+            for item in self.threeCyclesMap[i]:
+                ss = item + item
+                counts = []
+                for edge in [ss[0:2], ss[1:3], ss[2:4]]:
+                    counts.append(self.getEdgeInCycleCount(edge))
+                counts.sort()
+                key = "".join([str(count) for count in counts])
+                self.profileMap[i][key] = self.profileMap[i][
+                    key] + 1 if key in self.profileMap[i] else 1
+                self.profileMap[i] = dict(sorted(self.profileMap[i].items()))
+
+    def getEdgeInCycleCount(self, edge):
+        count = 0
+        for cycle in self.threeCycles:
+            if edge[0] in cycle and edge[1] in cycle:
+                count += 1
+        return count
+
+    def getChunkedVertices(self):
+        self.chunkedVertex = []
+        pointProfileDict = {}
+        for key, value in self.profileMap.items():
+            valueInJson = json.dumps(value)
+            if valueInJson in pointProfileDict:
+                pointProfileDict[valueInJson].append(key)
+            else:
+                pointProfileDict[valueInJson] = [key]
+        self.chunkedVertex = list(pointProfileDict.values())
+
+    def searchAutomorphism(self, chunkedVertex, index, permutationIndices):
+        if index >= len(chunkedVertex):
+            if self.checkIsAm(permutationIndices) == True:
+                self.am.append(permutationIndices)
+                # print("AM: ", permutationIndices)
+        else:
+            chunk = chunkedVertex[index]
+            purmuList = list(permutations(chunk))
+            for perm in purmuList:
+                for i, p in enumerate(chunk):
+                    permutationIndices[p] = perm[i]
+                self.searchAutomorphism(chunkedVertex, index + 1,
+                                        [p for p in permutationIndices])
+
+
+# solution9 = solution(9, dataset9)
+solution11 = solution(11, dataset11)
+# solution9.run()
+solution11.run()
